bfe5e276939b5b7fba2d18fe77d14d7b793a12d8
Luiz Carlos Abbott Galvão <luizcarlos_abbott@hotmail.com>
2020-02-13 16:51:12 -0300

    Capitulo 6

--- hubbub/grails-app/controllers/com/grailsinaction/PostController.groovy
+++ hubbub/grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -4,4 +4,34 @@ class PostController {
 
     static scaffold = Post
 
+//    def index() {
+//        if (!params.id) {
+//            params.id = "luiz"
+//        }
+//        redirect(action: 'timeline', params: params)
+//    }
+
+    def timeline() {
+        def user = User.findByLoginId(params.id)
+        if (!user) {
+            response.sendError(404)
+        } else {
+            [ user : user ]
+        }
+    }
+    def addPost() {
+        def user = User.findByLoginId(params.id)
+        if (user) {
+            def post = new Post(params)
+            user.addToPosts(post)
+            if (user.save(flush:true)) {
+                flash.message = "Successfully created Post"
+            } else {
+                flash.message = "Invalid or empty post"
+            }
+        } else {
+            flash.message = "Invalid User Id"
+        }
+        redirect(action: 'timeline', id: params.id)
+    }
 }
